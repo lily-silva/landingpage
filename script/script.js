20583af71122909f00e6f5daa29722e71d9289db
Lili <lilidrs13@gmail.com>
2019-04-08 11:40:01 +0100

    completed

--- script/script.js
+++ script/script.js
@@ -1,2 +1,18 @@
 // Single item slick carousel
 $('.single-item').slick();
+
+// Smoth Scrolling
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+            $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 80
+        }, 900, 'swing', function () {
+
+        });
+    });
+});
